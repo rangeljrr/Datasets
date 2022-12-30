4fc6832f0ce886135d0ff21dfc6de054d95f196a
Rodrigo Rangel Jr <rangeljrr@gmail.com>
2022-12-30 13:01:51 -0600

    Add files via upload

--- _support_functions.py
+++ _support_functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from datetime import datetime as dt
 
 def collect_API(API_name):
     """ 
@@ -18,3 +19,13 @@ def check_directory(directory):
     """ This function will check if the Fred directory exists """
     if os.path.isdir(directory) == False:
         os.mkdir(directory)
+        
+def create_date_range():
+    # Need to create a master file to store all indexes
+    now = dt.now()
+    min_month = '2010-01-01'
+    max_month = '{}-{}-01'.format(now.year, now.month)
+    months = pd.period_range(min_month, max_month, freq='M')
+    months = pd.DataFrame(months, columns=['date'])
+    
+    return months
